5da2942491f4b8ef2e1935779e5bc623bdb0747e
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-03-31 22:55:43 +0300

    Added base64 decode

--- CryptoLib/Base64.cpp
+++ CryptoLib/Base64.cpp
@@ -3,11 +3,15 @@
 
 std::string CryptoLib::Base64::Base64Enconde(
 	void* input_buffer,
-	size_t input_length
+	size_t input_length,
+	char* alphabet,
+	char padding
 )
 {
 	std::string result = "";
-	const char* BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	if (alphabet == nullptr) {
+		alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	}
 	uint8_t* buff = (uint8_t*)input_buffer;
 	int group_type = 0;
 	size_t index = 0;
@@ -39,11 +43,62 @@ std::string CryptoLib::Base64::Base64Enconde(
 			break;
 		}
 		group_type = (group_type + 1) % 4;
-		result += (char)BASE64_ALPHABET[value];
+		result += (char)alphabet[value];
 	}
 	while (result.length() % 4 != 0) 
 	{
-		result += "=";
+		result += padding;
 	}
 	return result;
+}
+
+void CryptoLib::Base64::Base64Decode(
+	void* input_buffer,
+	size_t input_length,
+	void* output_buffer,
+	size_t* output_length,
+	char* alphabet,
+	size_t alphabet_length,
+	char padding
+)
+{
+	if (alphabet == nullptr) {
+		alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	}
+	uint8_t assoc[256] = { 0 };
+	for (size_t i = 0; i < alphabet_length; i++) {
+		assoc[alphabet[i]] = i;
+	}
+
+	(*output_length) = (input_length * 3) / 4;
+	memset(output_buffer, 0, (*output_length));
+
+	uint8_t* in_buff = (uint8_t*)(input_buffer);
+	uint8_t* out_buff = (uint8_t*)(output_buffer);
+	int group_type = 0;
+	int out_index = 0;
+	for (size_t i = 0; i < input_length; i++) {
+		uint8_t value = assoc[in_buff[i]];
+		if (in_buff[i] == '=') break;
+		switch (group_type) {
+		case 0:
+			out_buff[out_index] |= (value << 2);
+			break;
+		case 1:
+			out_buff[out_index] |= ((value & 0x30) >> 4);
+			out_buff[out_index + 1] |= ((value & 0xF) << 4);
+			out_index++;
+			break;
+		case 2:
+			out_buff[out_index] |= ((value & 0x3C) >> 2);
+			out_buff[out_index + 1] |= ((value & 0x3) << 6);
+			out_index++;
+			break;
+		case 3:
+			out_buff[out_index] |= value;
+			out_index++;
+			break;
+		}
+		group_type = (group_type + 1) % 4;
+	}
 }
--- CryptoLib/Base64.h
+++ CryptoLib/Base64.h
@@ -6,7 +6,18 @@ namespace CryptoLib {
 	namespace Base64 {
 		__declspec(dllexport) std::string Base64Enconde(
 			void* input_buffer,
-			size_t input_length
+			size_t input_length,
+			char* alphabet = nullptr,
+			char padding = '='
+		);
+		__declspec(dllexport) void Base64Decode(
+			void* input_buffer,
+			size_t input_length,
+			void* output_buffer,
+			size_t* output_length,
+			char* alphabet = nullptr,
+			size_t alphabet_length = 64,
+			char padding = '='
 		);
 	};
 };
